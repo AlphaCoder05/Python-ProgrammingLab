a481ef4bfef788f3d4f50822f5785a103cd30d45
AlphaCoder05 <97780892+AlphaCoder05@users.noreply.github.com>
2022-05-29 00:09:44 +0530

    Add files via upload

---
+++ program 12.py
@@ -0,0 +1,6 @@
+#12.Write a Python script that displays first ten Mersenne numbers.
+print('First 10 Mersenne numbers are:')
+for a in range(1,11):
+    mersnum=2**a-1
+print(mersnum,end=' ')
+print()
