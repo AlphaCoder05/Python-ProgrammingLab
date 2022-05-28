7b611f4b444ea1e5ebfd28e5a6bf0f9c55793007
AlphaCoder05 <97780892+AlphaCoder05@users.noreply.github.com>
2022-05-29 00:14:58 +0530

    Add files via upload

---
+++ program 30.py
@@ -0,0 +1,9 @@
+#3. Write a program to find and display the sum of all the values which are ending with 3 from a list.
+L=[33,13,92,99,3,12]
+sum=0
+x=len(L)
+for i in range(0,x):
+    if type(L[i])==int:
+        if L[i]%10==3:
+            sum+=L[i]
+print(sum)   
