c0b54b3ca117b57ecb4db496e7ad65b7d5defc79
AlphaCoder05 <97780892+AlphaCoder05@users.noreply.github.com>
2022-05-28 20:37:33 +0530

    Add files via upload

---
+++ program 10.py
@@ -0,0 +1,6 @@
+#10.Write a program that accepts the age and print if one is eligible to vote or not.
+a=int(input('Enter your age:'))
+if a>=18:
+    print('You are eligible to vote')
+else:
+    print('You are not eligible to vote')
