a187d28c4db7cf04fd7a23ea074efe3fcf5778a7
AlphaCoder05 <97780892+AlphaCoder05@users.noreply.github.com>
2022-05-28 20:37:23 +0530

    Add files via upload

---
+++ program 9.py
@@ -0,0 +1,7 @@
+#9.Write a program to accept the height in cm and convert it into feet and inches.
+
+a=float(input('Enter your height in centimeters:'))
+Feet=a*0.032
+Inch=a*0.393
+print('Your height in feet is:', Feet)
+print('Your height in inch is:', Inch)
