8e359858767934bb201e3dd947f783d648da2cf2
AlphaCoder05 <97780892+AlphaCoder05@users.noreply.github.com>
2022-05-28 20:36:03 +0530

    Add files via upload

---
+++ program 5.py
@@ -0,0 +1,8 @@
+#5. Write a program to calculate simple interest.
+
+P=float(input('Enter the principal amount in ₹:'))
+R=float(input('Enter the rate of interest:'))
+T=float(input('Enter the time in years:'))
+SI=(P*R*T)/100
+print('The simple interest is:', SI,'₹')
+
